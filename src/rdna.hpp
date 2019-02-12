c7b965ade71113153761c105f3b2327f6759aa1a
Jackblack007 <Jackblack007@users.noreply.github.com>
2019-02-12 12:36:13 -0400

    Computing 2, Lab 2 Random numbers

---
+++ src/rdna.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+string randDNA( int seed, string word, int n){
+
+
+	if( word.length() < 1 )
+		return " ";
+
+	int min = 0;
+	int max = word.length() - 1;
+	string dna1 = "";
+
+
+	mt19937 eng1(seed);
+
+	uniform_int_distribution<> uniform(min, max);
+
+	for( int m = 0; m < n; m++)
+
+	{
+		int randomIndex = uniform(eng1);
+		dna1 += word[randomIndex];
+	}
+
+	return dna1;
+}
